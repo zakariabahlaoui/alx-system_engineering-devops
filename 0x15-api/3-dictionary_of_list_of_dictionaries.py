85a2a0d4cc04d8d86b66439c2dfee5c1de952c5a
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-07-30 03:48:39 +0100

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,41 @@
 #!/usr/bin/python3
-"""Script that uses REST API"""
+"""Script save information about all employees TODO list progress
+in the JSON format"""
+
 import json
 import requests
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+if __name__ == "__main__":
+    base_endpoint = f"https://jsonplaceholder.typicode.com/users"
+
+    # get all employees
+    employees = requests.get(base_endpoint).json()
+
+    # loop through all employees
+    dictionary = {}
+    for emp in employees:
+
+        # get employee id and username
+        id = emp.get("id")
+        username = emp.get("username")
+
+        # get todos for current employee
+        endpoint = f"{base_endpoint}/{id}/todos"
+        todos = requests.get(endpoint).json()
+
+        dictionary[id] = []
+        for todo in todos:
+            # save on dictionary
+            dictionary[id].append(
+                {
+                    "task": todo.get("title"),
+                    "completed": todo.get("completed"),
+                    "username": username,
+                }
+            )
+
+    # save dictionary that contains all employees data
+    filename = "todo_all_employees.json"
+    with open(filename, "w") as file:
+        json.dump(dictionary, file)
