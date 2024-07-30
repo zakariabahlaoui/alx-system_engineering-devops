cb109a98c991fdcdcdf3ddb7e700596bc6da86c6
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-07-30 03:32:07 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-"""Script that uses REST API"""
-import csv
+"""Script save information about employee TODO list progress
+in the CSV format"""
+
 import requests
 import sys
 
 
-def make_csv(users=None, todos=None):
-    """Turns payloads into CSV format"""
-    titles = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-
-    with open(sys.argv[1] + ".csv", "w") as f:
-        write = csv.DictWriter(f, fieldnames=titles, quoting=csv.QUOTE_ALL)
-        for i in todos:
-            write.writerow({"USER_ID": i.get("userId"),
-                            "USERNAME": users[0].get("username"),
-                            "TASK_COMPLETED_STATUS": i.get("completed"),
-                            "TASK_TITLE": i.get("title")})
-
-
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        args_id = {"id": sys.argv[1]}
-        users = requests.get("https://jsonplaceholder.typicode.com/users",
-                             params=args_id).json()
-        args_userid = {"userId": sys.argv[1]}
-        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
-                             params=args_userid).json()
-        todos_len = 0
-        todos_arr = []
-        for i in todos:
-            if i.get("completed"):
-                todos_arr.append(i)
-                todos_len += 1
+    id = sys.argv[1]
+    endpoint = f"https://jsonplaceholder.typicode.com/users/{id}"
 
-        make_csv(users, todos)
+    # get user name
+    name = requests.get(endpoint).json().get("username")
+
+    # get employee todos
+    endpoint += "/todos"
+    todos = requests.get(endpoint).json()
+
+    # save todos on CSV file
+    filename = f"{id}.csv"
+    with open(filename, "w") as file:
+        for task in todos:
+            completed = task.get("completed")
+            title = task.get("title")
+            file.write(f'"{id}","{name}","{completed}","{title}"\n')
