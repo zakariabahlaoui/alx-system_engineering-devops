9d4f41895a91872b18a3e3fbcb227f6c332aef02
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-07-31 12:59:25 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
 """Script that uses REST API"""
 import requests
-import json
-
-def get_employee_todo_progress(employee_id):
-    """Fetches employee data from the API and prints the task progress."""
-
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for error HTTP status codes
-
-        data = response.json()
-        employee_name = data['name']
-        todos = data['todos']
-
-        completed_tasks = [todo['title'] for todo in todos if todo['completed']]
-        total_tasks = len(todos)
-        completed_tasks_count = len(completed_tasks)
-
-        print(f"Employee {employee_name} is done with tasks({completed_tasks_count}/{total_tasks}):")
-        for task in completed_tasks:
-            print(f"\t{task}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+import sys
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py employee_id")
-        sys.exit(1)
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args = {"id": sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                             params=args).json()
+        args = {"userId": sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                             params=args).json()
+        todos_len = 0
+        todos_arr = []
+        for i in todos:
+            if i.get("completed"):
+                todos_arr.append(i)
+                todos_len += 1
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+        print("Employee {} is done with tasks({}/{}):".format(
+              users[0].get("name"), todos_len, len(todos)))
+
+        for i in todos_arr:
+            print("\t {}".format(i.get("title")))
