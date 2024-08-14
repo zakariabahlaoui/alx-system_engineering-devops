cf227649d87ec389da417dd092d3091cec55937c
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-14 03:16:50 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-"""queries the Reddit API"""
+"""
+queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns total subs"""
-    header = {"User-Agent": "Holberton"}
-    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
-    r = requests.get(url, headers=header)
-    if r.status_code == 200:
-        return r.json().get("data", None).get("subscribers", None)
-    else:
+    """get the number of subscribers for a given subreddit"""
+
+    # check argument
+    if not subreddit or not isinstance(subreddit, str):
         return 0
 
+    endpoint = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # Reddit API requires a User-Agent header
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-if __name__ == "__main__":
-    pass
+    # call API and get data
+    try:
+        response = requests.get(
+            endpoint, headers=headers, allow_redirects=False
+        )
+        if response.status_code == 200:
+            subscribers = (
+                response.json().get("data", {}).get("subscribers", None)
+            )
+            if subscribers:
+                return subscribers
+    except:
+        pass
+    return 0
