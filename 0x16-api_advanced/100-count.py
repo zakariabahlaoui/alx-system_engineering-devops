d0f24a8ba2a5ee5ce743371f96d0fc1d4a260ad5
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-14 03:14:40 +0100

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,65 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""
+queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords
+"""
+
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
+def count_words(subreddit, word_list, after=None, occurence_count=[]):
+    """prints a sorted count of given keywords"""
 
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
-    """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    if not after:
+        occurence_count = [0] * len(word_list)
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    endpoint = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    # reddit API requires a User-Agent header
+    headers = {"User-Agent": "Mozilla/5.0"}
+    # parameters to the request
+    params = {"after": after}
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+    response = requests.get(
+        endpoint, headers=headers, params=params, allow_redirects=False
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check occurrence of keywords in titles
+        for post in data.get("data").get("children"):
+            for title_word in post.get("data").get("title").split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == title_word.lower():
+                        occurence_count[i] += 1
+
+        after = data.get("data").get("after")
+        if after is None:  # no more next posts!
+            dup_list = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        dup_list.append(j)
+                        occurence_count[i] += occurence_count[j]
+
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if occurence_count[j] > occurence_count[i] or (
+                        word_list[i] > word_list[j]
+                        and occurence_count[j] == occurence_count[i]
+                    ):
+                        # Swap counts and keywords to sort
+                        # by count and keyword
+                        temp = occurence_count[i]
+                        occurence_count[i] = occurence_count[j]
+                        occurence_count[j] = temp
+                        temp = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = temp
+
+            for i in range(len(word_list)):
+                if (occurence_count[i] > 0) and i not in dup_list:
+                    print(f"{word_list[i].lower()}: {occurence_count[i]}")
+        else:
+            # call recursive function again to continue fetching next posts
+            count_words(subreddit, word_list, after, occurence_count)
